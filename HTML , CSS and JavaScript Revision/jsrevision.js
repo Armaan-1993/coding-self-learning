30acc9e3164fae85c8630410798d987b43c08257
armaan <armaanmathew88@gmail.com>
2020-09-20 12:29:47 +0530

    js

--- HTML , CSS and JavaScript Revision/jsrevision.js
+++ HTML , CSS and JavaScript Revision/jsrevision.js
@@ -37,7 +37,44 @@
 //     alert("We are the world!");
 // }
 
-function myFunction(name, age, sex) {
-    console.log(`My name is ${name} and i am ${age} years old`);
+// function myFunction(name, age, sex) {
+//     console.log(`My name is ${name} and i am ${age} years old`);
+// }
+// myFunction("Armaan", 26);
+
+// let name = "Armaan";
+// function myName() {
+//     let name = "Rehaan";
+//     console.log(name);
+// }
+// myName();
+
+// function runRandomNos(num) {
+//     alert(Math.ceil(Math.random() * num));
+// }
+// setInterval(runRandomNos(9), 1000);
+
+let myarray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+function addArray() {
+    let sum = 0;
+    for (let i = 0; i < myarray.length; i++) {
+        sum += myarray[i];
+    }
+    console.log(sum);
 }
-myFunction("Armaan", 26);
+addArray();
+
+let mynewArray = new Array(10);
+console.log(`The length of mynewArray is ${mynewArray.length}`);
+mynewArray.push(5);
+mynewArray.push(67);
+mynewArray[4] = 98;
+for (let i = 0; i < mynewArray.length; i++) {
+    if (mynewArray[i] === 67 || mynewArray[i] === 5) {
+        // console.log(`The index is ${mynewArray.indexOf(mynewArray[i])}`);
+        console.log(
+            `The index of 5 is at ${mynewArray.indexOf(mynewArray[i])}`
+        );
+    }
+    console.log(mynewArray[i]);
+}
