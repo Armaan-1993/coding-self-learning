c1f9b700c1a6a23c9646cdca618b23204c449047
armaan <armaanmathew88@gmail.com>
2020-12-05 00:31:34 +0530

    var let const

--- test/freecodecamp-JS/app.js
+++ test/freecodecamp-JS/app.js
@@ -1,11 +1,22 @@
-document.getElementById("my-button").addEventListener("click", myFunction);
-let x = document.getElementById("sex-bomb");
+// document.getElementById("my-button").addEventListener("click", myFunction);
+// let x = document.getElementById("sex-bomb");
 
-function myFunction() {
-    console.log(x.style.display);
-    // if (x.style.display === "block") {
-    //     x.style.display = "";
-    // }
+// function myFunction() {
+//     console.log(x.style.display);
+//     // if (x.style.display === "block") {
+//     //     x.style.display = "";
+//     // }
 
-    // e.preventDefault();
-}
+//     // e.preventDefault();
+// }
+
+// var a = "name";
+// var b = "age";
+// a = "sex";
+// if (a) {
+//     a = "sex";
+//     console.log(a);
+// }
+
+// console.log(name);
+// var name = "armaan";
